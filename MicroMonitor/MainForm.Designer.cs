68c34caefc613a7fd06591e21288968d42d31934
ashes999 <ashes999@yahoo.com>
2016-06-23 11:44:12 -0400

    Basic pinging, using app.config for computer names

---
+++ MicroMonitor/MainForm.Designer.cs
@@ -0,0 +1,102 @@
+﻿namespace MicroMonitor
+{
+    partial class MainForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.uxComputerList = new System.Windows.Forms.ListView();
+            this.columnHeader1 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnHeader2 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.uxAdd = new System.Windows.Forms.Button();
+            this.uxDelete = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // uxComputerList
+            // 
+            this.uxComputerList.Columns.AddRange(new System.Windows.Forms.ColumnHeader[] {
+            this.columnHeader1,
+            this.columnHeader2});
+            this.uxComputerList.Location = new System.Drawing.Point(12, 42);
+            this.uxComputerList.Name = "uxComputerList";
+            this.uxComputerList.Size = new System.Drawing.Size(760, 507);
+            this.uxComputerList.TabIndex = 0;
+            this.uxComputerList.UseCompatibleStateImageBehavior = false;
+            this.uxComputerList.View = System.Windows.Forms.View.Details;
+            // 
+            // columnHeader1
+            // 
+            this.columnHeader1.Text = "Computer";
+            this.columnHeader1.Width = 259;
+            // 
+            // columnHeader2
+            // 
+            this.columnHeader2.Text = "Status";
+            this.columnHeader2.Width = 120;
+            // 
+            // uxAdd
+            // 
+            this.uxAdd.Location = new System.Drawing.Point(12, 13);
+            this.uxAdd.Name = "uxAdd";
+            this.uxAdd.Size = new System.Drawing.Size(75, 23);
+            this.uxAdd.TabIndex = 1;
+            this.uxAdd.Text = "Add";
+            this.uxAdd.UseVisualStyleBackColor = true;
+            // 
+            // uxDelete
+            // 
+            this.uxDelete.Location = new System.Drawing.Point(697, 13);
+            this.uxDelete.Name = "uxDelete";
+            this.uxDelete.Size = new System.Drawing.Size(75, 23);
+            this.uxDelete.TabIndex = 2;
+            this.uxDelete.Text = "Delete";
+            this.uxDelete.UseVisualStyleBackColor = true;
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(784, 561);
+            this.Controls.Add(this.uxDelete);
+            this.Controls.Add(this.uxAdd);
+            this.Controls.Add(this.uxComputerList);
+            this.Name = "MainForm";
+            this.Text = "Micro Monitor";
+            this.Load += new System.EventHandler(this.MainForm_Load);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ListView uxComputerList;
+        private System.Windows.Forms.ColumnHeader columnHeader1;
+        private System.Windows.Forms.ColumnHeader columnHeader2;
+        private System.Windows.Forms.Button uxAdd;
+        private System.Windows.Forms.Button uxDelete;
+    }
+}
+
---
+++ MicroMonitor/MainForm.cs
@@ -0,0 +1,129 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Configuration;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Net;
+using System.Net.NetworkInformation;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace MicroMonitor
+{
+    public partial class MainForm : Form
+    {
+        List<string> computerNames = new List<string>();
+        List<string> statuses = new List<string>();
+
+        Timer refreshTimer = new Timer();
+
+        public MainForm()
+        {
+            InitializeComponent();
+        }
+
+        private void MainForm_Load(object sender, EventArgs e)
+        {
+            var names = ConfigurationManager.AppSettings["Computers"];
+            if (!string.IsNullOrEmpty(names))
+            {
+                computerNames = names.Split(new char[] { ',' }).ToList();
+                foreach (var computer in computerNames)
+                {
+                    statuses.Add("Loading ...");
+                }
+
+                RefreshDisplay();
+            }
+
+            refreshTimer.Interval = (int)TimeSpan.FromMinutes(1).TotalMilliseconds;
+            refreshTimer.Tick += (innerSender, eventArgs) =>
+            {
+                PingAndUpdateStatuses();
+            };
+
+            refreshTimer.Start();
+            // Do it immediately
+            PingAndUpdateStatuses();
+        }
+
+        private void PingAndUpdateStatuses()
+        {
+            // Ping all
+            this.statuses.Clear();
+
+            foreach (var computer in computerNames)
+            {
+                var status = PingHost(computer);
+                statuses.Add(StatusToMessage(status));
+            }
+
+            // Update status
+            this.RefreshDisplay();
+        }
+
+        private string StatusToMessage(IPStatus status)
+        {
+            switch (status)
+            {
+                case IPStatus.Success:
+                    return "Online";
+                default:
+                    return "Offline";
+            }
+        }
+
+        public IPStatus PingHost(string host)
+        {
+            //string to hold our return messge
+            string returnMessage = string.Empty;
+
+            //IPAddress instance for holding the returned host
+            //set the ping options, TTL 128
+            PingOptions pingOptions = new PingOptions(128, true);
+
+            //create a new ping instance
+            Ping ping = new Ping();
+
+            //32 byte buffer (create empty)
+            byte[] buffer = new byte[32];
+
+            try
+            {
+                PingReply pingReply = ping.Send(host, 1000, buffer, pingOptions);
+
+                if (pingReply != null)
+                {
+                    return pingReply.Status;
+                }
+                else
+                {
+                    return IPStatus.Unknown;
+                }
+            }
+            catch (PingException)
+            {
+                return IPStatus.Unknown;
+            }
+            catch (SocketException)
+            {
+                return IPStatus.Unknown;
+            }
+        }
+
+        private void RefreshDisplay()
+        {
+            this.uxComputerList.Items.Clear();
+            for (int i = 0; i < computerNames.Count; i++)
+            {
+                var computer = computerNames[i];
+                var status = statuses[i];
+                this.uxComputerList.Items.Add(new ListViewItem(new string[] { computer, status }));
+            }
+        }
+    }
+}

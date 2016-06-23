eae6c8a8dd781e99c2a23773dfbea2c353664346
ashes999 <ashes999@yahoo.com>
2016-06-23 11:55:12 -0400

    Implemented app as tray icon Context menu, icon switches if any machines go offline.

--- MicroMonitor/MainForm.cs
+++ MicroMonitor/MainForm.cs
@@ -16,8 +16,11 @@ namespace MicroMonitor
 {
     public partial class MainForm : Form
     {
-        List<string> computerNames = new List<string>();
-        List<string> statuses = new List<string>();
+        private NotifyIcon trayIcon;
+        private ContextMenu trayMenu;
+
+        private List<string> computerNames = new List<string>();
+        private List<string> statuses = new List<string>();
 
         Timer refreshTimer = new Timer();
 
@@ -28,6 +31,24 @@ namespace MicroMonitor
 
         private void MainForm_Load(object sender, EventArgs e)
         {
+            // Create a simple tray menu with only one item.
+            trayMenu = new ContextMenu();
+            trayMenu.MenuItems.Add("Restore", (s2, e2) => this.Show());
+            trayMenu.MenuItems.Add("Exit", (s2, e2) => Application.Exit());
+
+            // Create a tray icon. In this example we use a
+            // standard system icon for simplicity, but you
+            // can of course use your own custom icon too.
+            trayIcon = new NotifyIcon();
+            trayIcon.Text = "Micro Monitor";
+            trayIcon.Icon = new Icon(SystemIcons.Application, 40, 40);
+            trayIcon.DoubleClick += (s2, e2) => this.Show();
+
+            // Add menu to tray icon and show it.
+            trayIcon.ContextMenu = trayMenu;
+            trayIcon.Visible = true;
+
+            // Load the computers and refresh status
             var names = ConfigurationManager.AppSettings["Computers"];
             if (!string.IsNullOrEmpty(names))
             {
@@ -47,6 +68,7 @@ namespace MicroMonitor
             };
 
             refreshTimer.Start();
+
             // Do it immediately
             PingAndUpdateStatuses();
         }
@@ -64,6 +86,12 @@ namespace MicroMonitor
 
             // Update status
             this.RefreshDisplay();
+
+            // Update tray icon
+            if (statuses.Any(s => s == "Offline"))
+            {
+                trayIcon.Icon = new Icon(SystemIcons.Error, 40, 40);
+            }
         }
 
         private string StatusToMessage(IPStatus status)
@@ -124,6 +152,24 @@ namespace MicroMonitor
                 var status = statuses[i];
                 this.uxComputerList.Items.Add(new ListViewItem(new string[] { computer, status }));
             }
+        }
+
+        protected override void OnLoad(EventArgs e)
+        {
+            Visible = false; // Hide form window.
+            ShowInTaskbar = false; // Remove from taskbar.
+
+            base.OnLoad(e);
+        }
+
+        protected override void OnFormClosing(FormClosingEventArgs e)
+        {
+            if (e.CloseReason != CloseReason.WindowsShutDown && e.CloseReason != CloseReason.ApplicationExitCall)
+            {
+                e.Cancel = true;                
+            }
+
+            this.Hide();
         }
     }
 }
